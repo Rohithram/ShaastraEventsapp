366093a06b4224de8fce2e529d6d2a17bffe6692
rohithram <ram234historyalex@gmail.com>
2017-06-05 23:07:24 +0530

    added google maps

--- app/src/main/java/io/rohithram/shaastraeventsapp/Main3Activity.java
+++ app/src/main/java/io/rohithram/shaastraeventsapp/Main3Activity.java
@@ -20,6 +20,7 @@ String code;
     TextView tv_prizemoney;
     TextView tv_coordname;
     TextView tv_call;
+    TextView tv_map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,9 @@ String code;
         tv_prizemoney =(TextView)findViewById(R.id.tv_prizemoney);
         tv_coordname =(TextView)findViewById(R.id.tv_cordname);
         tv_call = (TextView)findViewById(R.id.tv_call);
+        tv_map = (TextView)findViewById(R.id.tv_map);
         tv_call.setOnClickListener(this);
+        tv_map.setOnClickListener(this);
         Intent i2 = getIntent();
         code = i2.getStringExtra("code");
         switch (code){
@@ -104,23 +107,86 @@ String code;
 
     @Override
     public void onClick(View v) {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        switch (code){
-            case "11":   callIntent.setData(Uri.parse("tel:+919840563641"));break;
-            case "21":   callIntent.setData(Uri.parse("tel:+919841563641"));break;
-            case "31":   callIntent.setData(Uri.parse("tel:+918755465321"));break;
-            case "41":   callIntent.setData(Uri.parse("tel:+919841363641"));break;
-            case "12":   callIntent.setData(Uri.parse("tel:+919040563641"));break;
-            case "22":   callIntent.setData(Uri.parse("tel:+919840745536"));break;
-            case "32":   callIntent.setData(Uri.parse("tel:+919840563641"));break;
-            case "42":   callIntent.setData(Uri.parse("tel:+919845563641"));break;
+        switch (v.getId()) {
+            case R.id.tv_call:  Intent callIntent = new Intent(Intent.ACTION_CALL);
+                                switch (code) {
+                                    case "11":
+                                        callIntent.setData(Uri.parse("tel:+919840563641"));
+                                        break;
+                                    case "21":
+                                        callIntent.setData(Uri.parse("tel:+919841563641"));
+                                        break;
+                                    case "31":
+                                        callIntent.setData(Uri.parse("tel:+918755465321"));
+                                        break;
+                                    case "41":
+                                        callIntent.setData(Uri.parse("tel:+919841363641"));
+                                        break;
+                                    case "12":
+                                        callIntent.setData(Uri.parse("tel:+919040563641"));
+                                        break;
+                                    case "22":
+                                        callIntent.setData(Uri.parse("tel:+919840745536"));
+                                        break;
+                                    case "32":
+                                        callIntent.setData(Uri.parse("tel:+919840563641"));
+                                        break;
+                                    case "42":
+                                        callIntent.setData(Uri.parse("tel:+919845563641"));
+                                        break;
 
-        }
+                                }
 
-        if (ActivityCompat.checkSelfPermission(Main3Activity.this,
-                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        startActivity(callIntent);
+                                    if (ActivityCompat.checkSelfPermission(Main3Activity.this,
+                                            Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                        return;
+                                    }
+                                    startActivity(callIntent);
+                                    break;
+
+
+            case R.id.tv_map:   Intent intent = new Intent(this, Mapactivity.class);
+                                intent.putExtra("loc",tv_locval.getText());
+
+                                switch (code) {
+                                                    case "11":  intent.putExtra("lat",String.valueOf(12.990055));
+                                                                intent.putExtra("lng",String.valueOf(80.230344));
+                                                        break;
+                                                    case "21":
+
+                                                        intent.putExtra("lat",String.valueOf(12.992964));
+                                                        intent.putExtra("lng",String.valueOf(80.232450));
+                                                        break;
+                                                    case "31":
+                                                        intent.putExtra("lat",String.valueOf(12.991773));
+                                                        intent.putExtra("lng",String.valueOf(80.232156));
+                                                        break;
+                                                    case "41":
+                                                        intent.putExtra("lat",String.valueOf(12.990055));
+                                                        intent.putExtra("lng",String.valueOf(80.230344));
+                                                        break;
+                                                    case "12": intent.putExtra("lat",String.valueOf(12.990055));
+                                                        intent.putExtra("lng",String.valueOf(80.230344));
+                                                        break;
+                                                    case "22":
+                                                        intent.putExtra("lat",String.valueOf(12.989344));
+                                                        intent.putExtra("lng",String.valueOf(80.237795));
+                                                        break;
+                                                    case "32":
+                                                        intent.putExtra("lat",String.valueOf(12.989524));
+                                                        intent.putExtra("lng",String.valueOf(80.232));
+                                                        break;
+                                                    case "42":
+                                                        intent.putExtra("lat",String.valueOf(12.990055));
+                                                        intent.putExtra("lng",String.valueOf(80.230344));
+                                                        break;
+
+                                                }
+                                                    intent.putExtra("code",code);
+                                                    startActivity(intent);
+                                            break;
+                                }
+
+
     }
 }
---
+++ app/src/main/java/io/rohithram/shaastraeventsapp/Mapactivity.java
@@ -0,0 +1,61 @@
+package io.rohithram.shaastraeventsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+        public class Mapactivity extends FragmentActivity implements OnMapReadyCallback {
+
+            private GoogleMap mMap;
+            String code;
+            String lat;
+            String lng;
+            String loc;
+            Double lat1,lng1;
+            final static float INITIAL_ZOOM = 17.0f;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_mapactivity);
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(this);
+
+                Intent map = getIntent();
+                code = map.getStringExtra("code");
+                lat = map.getStringExtra("lat");
+                lng = map.getStringExtra("lng");
+                loc = map.getStringExtra("loc");
+                lat1= Double.parseDouble(lat);
+                lng1=Double.parseDouble(lng);// Make use of autoboxing.  It's also easier to read.
+            }
+            /**
+             * Manipulates the map once available.
+             * This callback is triggered when the map is ready to be used.
+             * This is where we can add markers or lines, add listeners or move the camera. In this case,
+             * we just add a marker near Sydney, Australia.
+             * If Google Play services is not installed on the device, the user will be prompted to install
+             * it inside the SupportMapFragment. This method will only be triggered once the user has
+             * installed Google Play services and returned to the app.
+             */
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+
+                // Add a marker on the location of the event which was passed as an extra in the intent
+                LatLng eventLoc = new LatLng(lat1,lng1);
+                mMap.addMarker(new MarkerOptions().position(eventLoc).title(loc));
+
+                // Move the camera and zoom it in
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(eventLoc,INITIAL_ZOOM));
+            }
+        }
